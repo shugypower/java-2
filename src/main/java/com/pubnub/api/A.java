ed840076382e7a49c09f65458e31e6c539fe6812
shugypower <41370093+shugypower@users.noreply.github.com>
2018-07-18 14:59:52 +0300

    Update A.java

--- src/main/java/com/pubnub/api/A.java
+++ src/main/java/com/pubnub/api/A.java
@@ -1,7 +1,16 @@
 class A{
   void foo(){
+    int x = 2;
+    if (x > 1)
     goo();
   }
+  
+  void foo2(){
+    int x = 2;
+    if (x > 1)
+    goo();
+  }
+  
   
   void goo(){
     int x = 123;
